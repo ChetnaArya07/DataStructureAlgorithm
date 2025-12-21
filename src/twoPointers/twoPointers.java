bb66561763597bfbad1e014f92f79d47d871505a
Chetna-07 <aryanoone098@gmail.com>
2025-12-21 21:33:29 +0530

    Two pointers : reverse an character array

--- src/twoPointers/twoPointers.java
+++ src/twoPointers/twoPointers.java
@@ -12,21 +12,6 @@ public class twoPointers {
 
     }
 
-    public static char[] reverseCharArray(char[] nums) {
-        char[] revArray = new char[nums.length];
-       int start = 0;
-       int end = nums.length-1;
-        while(start <= end){
-            revArray[start] = nums[end];
-            revArray[end] = nums[start];
-            start++;
-            end--;
-        }
-
-        System.out.println("reverse char array : "+ Arrays.toString(revArray));
-        return revArray;
-    }
-
     public static char[] reverseCharArray2(char[] nums) {
         int start = 0;
         int end = nums.length-1;
