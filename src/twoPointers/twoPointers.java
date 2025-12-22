6c063e56fd15b2f7d971714a48ff52ecf6571017
Chetna-07 <aryanoone098@gmail.com>
2025-12-22 22:52:31 +0530

    Two pointers : two sum for sorted array

--- src/twoPointers/twoPointers.java
+++ src/twoPointers/twoPointers.java
@@ -9,7 +9,9 @@ public class twoPointers {
 
         char[]  arr = {'h', 'e', 'l', 'l', 'o'};
         char[] result = reverseCharArray2(arr);
-
+        int [] nums = {2, 7, 11, 15};
+        int target = 9;
+        twoSumSorted(nums,target);
     }
 
     public static char[] reverseCharArray2(char[] nums) {
@@ -25,6 +27,24 @@ public class twoPointers {
 
         System.out.println("reverse char array : "+ Arrays.toString(nums));
         return nums;
+    }
+
+    public static int[] twoSumSorted(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int sum = nums[start] + nums[end];
+
+            if (sum == target) {
+                return new int[]{start, end};
+            } else if (sum > target) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+        return new int[]{-1, -1};
     }
 
 
